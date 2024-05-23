e9acca5181287f79c5a9397d9a4bed783edd6956
bhagwan48 <pabhiraaz48@gmail.com>
2024-05-23 05:49:07 +0530

    Project Completed

---
+++ src/Pages/CategoryPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import Blogs from '../components/Blogs';
+import Pagination from '../components/Pagination';
+
+const CategoryPage = () => {
+
+    const navigation = useNavigate();
+    const location = useLocation();
+    const category = location.pathname.split("/").at(-1);
+  return (
+    <div>
+        <Header />
+        <div>
+            <button
+            onClick={() => navigation(-1)}
+            >
+                Back
+            </button>
+            <h2>
+                Blogs on <span>{category}</span>
+            </h2>
+        </div>
+        <Blogs />
+        <Pagination />
+    </div>
+  )
+}
+
+export default CategoryPage
